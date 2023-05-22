f5cef461671cd1bc7ceb3f6fc376c2b7a5014962
Dimuthu.D <Dimuthu.D@creativesoftware.com>
2023-05-22 10:17:42 +0530

    Remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,46 +4,39 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatGridListModule,
-  MatToolbarModule,
-  MatCardModule,
-  MatSidenavModule,
+  MatAutocompleteModule,
   MatButtonModule,
-  MatLineModule,
-  MatListModule,
+  MatCardModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatGridListModule,
   MatIconModule,
   MatInputModule,
-  MatFormFieldModule,
-  MatDatepickerModule,
+  MatListModule,
   MatNativeDateModule,
-  MatChipsModule,
-  MatAutocompleteModule, MatSelectModule, MatSnackBarModule
+  MatSelectModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatToolbarModule
 } from '@angular/material';
 import {EmployeeMgtComponent} from './employee-mgt/employee-mgt.component';
 import {SkillMgtComponent} from './skill-mgt/skill-mgt.component';
 import {AppRoutingModule} from './app-routing.module';
-import {EmpItemComponent} from './emp-item/emp-item.component';
 import {UpdateEmpComponent} from './update-emp/update-emp.component';
 import {EmpProfileComponent} from './emp-profile/emp-profile.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AddEmpComponent} from './add-emp/add-emp.component';
-import {SkillChipsComponent} from './skill-chips/skill-chips.component';
 import {HttpClientModule} from '@angular/common/http';
-import {SkillSelectorComponent} from './skill-selector/skill-selector.component';
-import {InputErrorsExampleComponent} from './input-errors-example/input-errors-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeMgtComponent,
     SkillMgtComponent,
-    EmpItemComponent,
     UpdateEmpComponent,
     EmpProfileComponent,
     AddEmpComponent,
-    SkillChipsComponent,
-    SkillSelectorComponent,
-    InputErrorsExampleComponent
   ],
   imports: [
     BrowserModule,
